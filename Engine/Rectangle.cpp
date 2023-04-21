3c373e6ceb868128810d3639eae9ff3e34e622e8
yliu <yliu.yb@gmail.com>
2023-04-21 16:03:30 +0800

    add rectangle

--- Engine/Rectangle.cpp
+++ Engine/Rectangle.cpp
@@ -1,1 +1,10 @@
 #include "Rectangle.h"
+
+Rectangle::Rectangle(int top, int left, int right, int bottom)
+	:
+	topLeft(left, top),
+	bottomRight(right, bottom)
+{
+}
+
+
--- Engine/Rectangle.h
+++ Engine/Rectangle.h
@@ -1,6 +1,14 @@
 #pragma once
+#include"Vec2.h"
+
 class Rectangle
 {
+public:
+	Rectangle() = default;
+	Rectangle(int top, int left, int right, int bottom);
 
+public:
+	Vec2 topLeft;
+	Vec2 bottomRight;
 };
 
